97738f5d187eabf46d5d40a581555401a1cd1de4
JollyNewton <jollycnewton@gmail.com>
2023-03-16 05:56:44 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,9 @@
  * main - Printing a line with printf command
  * Return: 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
 
