eeb4c7bb8f578d5eeeacdfd44740b2d00678d21e
JollyNewton <jollycnewton@gmail.com>
2023-03-16 04:36:24 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - Print with puts function program
+ * Return: 0(Succeess)
+ */ 
+int main(void)
+{
+puts("\"Programing is all abot progress");
+return (0);
+}
