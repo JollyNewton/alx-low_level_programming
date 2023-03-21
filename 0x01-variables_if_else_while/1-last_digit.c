a92ab1bd3ad8247560cd53629436ab970e6a2978
JollyNewton <jollycnewton@gmail.com>
2023-03-21 00:41:19 -0700

    Checking with betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a randomly generated number 
- * nad whether it is greater than 6, less than 6, or 0.
+ * main - Prints the last digit of a randomly generated number
+ * nad whether it is greater than 6, less than 6, or 0
  *
  * Return: Always 0.
  */
@@ -17,8 +17,7 @@ int main(void)
 
 	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",
-			       	n, n % 10);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints all possible different combination of two digits 
+ * main - Prints all possible different combination of two digits
  * Return: Always 0 (Success)
  */
 int main(void)
