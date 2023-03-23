14d94c6c902f0bba60b81d2aba63757d6ad02019
JollyNewton <jollycnewton@gmail.com>
2023-03-23 02:16:52 -0700

    Correction Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio>
 
 /**
  * main - Test function for positive or negative
@@ -8,7 +9,8 @@ int main(void)
 {
 	int i;
 
-	i = 0
-		positive_or_negative(i);
-	return (0);
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
 }
