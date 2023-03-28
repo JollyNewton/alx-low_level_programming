11a135b83631d00897a6cb65606c18f01dc24343
JollyNewton <jollycnewton@gmail.com>
2023-03-28 10:45:39 -0700

    Correcting Task 7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,29 +1,15 @@
-#include "main.h"
-/**
- * puts2 - function should print only one character out of two
- * starting with the first one
- * @str: input
- * Return: print
- */
-void puts2(char *str)
-{
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+#include "holberton.h"
 
-	while (*y != '\0')
-	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0 ; 0 <= t ; o++)
-	{
-		if (0 % 2 == 0)
-		{
-			_putchar(str[0]);
-		}
-	}
-	_putchar('\n');
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+    puts2(str);
+    return (0);
 }
