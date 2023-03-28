c44fdb668d0309d3a8ded9e957f90fc988664015
JollyNewton <jollycnewton@gmail.com>
2023-03-28 10:13:38 -0700

    Task 10 corrected

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,7 @@
  * 
  * Return: the int converted from the string
  */
-int _atoi(char *s)
+int _atoi(char *s);
 {
 	int i, d, n, len, f, digit;
 
@@ -34,7 +34,7 @@ int _atoi(char *s)
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			f = 0
+			f = 0;
 		}
 		i++;
 	}
